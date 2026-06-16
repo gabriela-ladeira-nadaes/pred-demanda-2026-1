0672abbe75dd5d835c3e8baf2b4be107bda513f9
Gabriel Leite <leite06@Hotmail.com>
2026-06-16 20:33:16 -0300

    fix: naming

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-def (path: str) -> pd.DataFrame:
+def load_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
+
+def validate_data(data: pd.DataFrame) -> Boolean:
+    return true
