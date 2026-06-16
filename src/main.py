121def83cb1da0fc2e677ef7f6fb1b7841cd5474
Thiago Nascimento <thiago.nascimento@mobilicidade.com.br>
2026-06-16 20:34:19 -0300

    Etapa 2 - imports

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+import data.data_loader as loaderData
+import evaluation.metrics as metrics
+import models.model as model
+import preprocessing.transform as transform
+import training.train as train
+import utils.config as config
+
+
 def main():
     pass
 
