fc1ebf7fd8857600a19c08a0633f3d8b68dd37b7
Thiago Nascimento <thiago.nascimento@mobilicidade.com.br>
2026-06-16 20:57:32 -0300

    Novos imports

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from data.data_loader import load_data, validate_data
 from preprocessing.transform import clean_data, split_data
+from training.train import train_model
 import pandas as pd
 
 DATA_PATH = Path("data/walmart_dataset_sales.csv")
--- src/training/train.py
+++ src/training/train.py
@@ -2,5 +2,6 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
 def train_model(x_train: pd.DataFrame, y_train: pd.Series) -> RandomForestRegressor:
+    
     return model
     
