e019c98bd5b1a9f1966ced4e35dcb1f804063530
Gabriela Ladeira Nadaes <gabriela.nadaes@gmail.com>
2026-06-16 20:38:05 -0300

    Etapa 3 - Definição do main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-import data.data_loader as loaderData
-import evaluation.metrics as metrics
-import models.model as model
-import preprocessing.transform as transform
-import training.train as train
-import utils.config as config
+<<<<<<< HEAD
+from pathlib import Path
+from data_loader import load_data
+from transform import clean_data, split_data
 
+DATA_PATH = Path("data/walmart_dataset_sales.csv")
 
-def main():
-
-    loaderData.load_data()
-    pass
+def main() -> None:
+    data = load_data(DATA_PATH)
+    data = clean_data(data)
+    
+    X_treino, X_teste, y_treino, y_teste = split_data(dados)
 
 if __name__ == "__main__":
     main()
