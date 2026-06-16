6472a918bbfb40820c4c33992b8e57ab72a7a2c7
Thiago Nascimento <thiago.nascimento@mobilicidade.com.br>
2026-06-16 20:46:27 -0300

    Ajustando o main

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 from pathlib import Path
-from data_loader import load_data
-from transform import clean_data, split_data
+from data.data_loader import load_data, validate_data
+from preprocessing.transform import clean_data, split_data
+import pandas as pd
 
 DATA_PATH = Path("data/walmart_dataset_sales.csv")
 
 def main() -> None:
-    data = load_data(DATA_PATH)
+    data: pd.DataFrame = load_data(DATA_PATH)
+    if not validate_data(data):
+        print("Base de daos nao validade")
+        return
+        
     data = clean_data(data)
     
-    X_treino, X_teste, y_treino, y_teste = split_data(dados)
+    X_treino, X_teste, y_treino, y_teste = split_data(data)
 
 if __name__ == "__main__":
     main()
