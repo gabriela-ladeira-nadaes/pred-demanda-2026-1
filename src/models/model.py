3f6f357aa2ea7bb63b6db2dc468db9ee957e43c6
luis_lascasas <124100587+dudumlc@users.noreply.github.com>
2026-06-17 20:44:55 -0300

    edit função save_model

--- src/models/model.py
+++ src/models/model.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-def save_model(train_data: pd.Series, test_data: pd.Series, model: RandomForestRegressor):
+def save_model(caminho_arquivo: str, model: RandomForestRegressor) -> None:
     pass
